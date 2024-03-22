7d05a5722226fc04275d0b0776bbe325ce5623d0
trycatchfun <162821256+trycatchfun@users.noreply.github.com>
2024-03-22 19:44:52 +0000

    0.0.34

--- Week1/SupervisedClassifier.py
+++ Week1/SupervisedClassifier.py
@@ -2,4 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegr
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics impo
