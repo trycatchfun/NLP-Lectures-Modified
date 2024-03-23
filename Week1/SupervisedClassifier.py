020d620ea4fa4b6747c70136c47ce24edc763d6d
trycatchfun <162821256+trycatchfun@users.noreply.github.com>
2024-03-23 08:52:19 +0000

    LICENSE.md

--- Week1/SupervisedClassifier.py
+++ Week1/SupervisedClassifier.py
@@ -3,4 +3,4 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics impo
+from sklearn.metrics import classification_report, confusion_matri
