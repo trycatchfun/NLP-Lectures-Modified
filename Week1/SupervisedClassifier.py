662880c19b707b520d411e7fe76c26c11edfba4d
trycatchfun <162821256+trycatchfun@users.noreply.github.com>
2024-03-23 10:33:03 +0000

    v1.8.1

--- Week1/SupervisedClassifier.py
+++ Week1/SupervisedClassifier.py
@@ -4,4 +4,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from 
+from sklearn.naive_bayes import GaussianNB
+f
