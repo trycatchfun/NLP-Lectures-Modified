45dcc0d83d49fa2285f4ce05ded54b445e92280d
trycatchfun <162821256+trycatchfun@users.noreply.github.com>
2024-03-23 08:54:17 +0000

    cgroup: notify_on_release may not be triggered in some cases

--- Week1/SupervisedClassifier.py
+++ Week1/SupervisedClassifier.py
@@ -3,4 +3,5 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matri
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from 
