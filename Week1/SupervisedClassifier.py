61c8057a91e57e80e6c8fb68abf9de75c70a7ded
trycatchfun <162821256+trycatchfun@users.noreply.github.com>
2024-03-23 11:58:12 +0000

    Prepare 1.2.2 release

--- Week1/SupervisedClassifier.py
+++ Week1/SupervisedClassifier.py
@@ -5,4 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassif
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+# imp
