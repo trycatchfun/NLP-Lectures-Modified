41db7c6aafd4e61f601154feddf4eef0045702d2
trycatchfun <162821256+trycatchfun@users.noreply.github.com>
2024-04-16 08:29:26 +0000

    Fix golint

--- Week2/ManualNN.py
+++ Week2/ManualNN.py
@@ -5,4 +5,8 @@ input_data = np.array([5, 7, 8, 1])
 
 weights = {'node0': np.array([1, 1]),
            'node1': np.array([-1, 1]),
-           'output': np.array([2, -1]
+           'output': np.array([2, -1])}
+
+
+# Activation Function
+def Re
