f4587708bd3164019ac10e4b8b92c1bb2215053c
trycatchfun <162821256+trycatchfun@users.noreply.github.com>
2024-04-22 05:25:56 +0000

    msm: mdss: Allow smp change during composition switch

--- Week2/ManualNN.py
+++ Week2/ManualNN.py
@@ -23,4 +23,5 @@ def predict_with_NN(input_data_row, weights):
     # Calculation for node 0 value
     node_0_input = (input_data_row * weights['node0']).sum()
     print("Node 0 in hidden layer before activation function: %d" % node_0_input)
-    node_0_output = ReLu(node_0_input
+    node_0_output = ReLu(node_0_input)
+    print("Node 0 in hidden l
