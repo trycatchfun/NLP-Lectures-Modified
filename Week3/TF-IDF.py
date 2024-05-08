ad79093f44b946217c81c56d700c18cc248c2aa1
trycatchfun <162821256+trycatchfun@users.noreply.github.com>
2024-05-08 17:59:14 +0000

    fixed NullPointerException

--- Week3/TF-IDF.py
+++ Week3/TF-IDF.py
@@ -1,3 +1,34 @@
 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+import math
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+
+# Given Corpus with different documents
+corpus = {1: 'i am student of computer engineering at the university of guilan',
+          2: 'i am studying natural language processing right now'}
+
+# Create a bag of words for each documents
+BoW = []
+# Split each document
+for row in corpus:
+    BoW.append(corpus[row].split(' '))
+    Num_document = row  # use it for making sets
+# print(BoW)
+# print(Num_document)
+
+# Remove any duplicate words
+unique_Words = set(BoW[0]).union(set(BoW[1]))
+# print(uniqueWord)
+
+# Create Document-Word matrix
+# Sort unique words for better visualization
+unique_Words = sorted(unique_Words)
+# dict.fromkeys(x=keys, y=value)
+Doc1 = dict.fromkeys(unique_Words, 0)
+# Count word occurrence in Doc
+for word in BoW[0]:
+    Doc1[word] += 1
